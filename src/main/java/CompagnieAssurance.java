c665d40f84e5cde4298121b792eea602c5d5cbad
Marine Bouvet <marinebouvetpro@gmail.com>
2017-12-11 16:50:41 +0100

    Compagnie classe complétées

--- src/main/java/CompagnieAssurance.java
+++ src/main/java/CompagnieAssurance.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
 
 public class CompagnieAssurance {
+	private String nom;
+	public ArrayList<Personne> personne;
+	
+	public int obtenirNombreDeClients()
+	{
+		int nbClients =0 ;
+		return nbClients;
+	}
+	
+	public int obtenirNombreDeProspects()
+	{
+		int nbProspects =0 ;
+		return nbProspects;
+	}
+	
+	public int obtenirNombreDeContrats()
+	{
+		int nbContrats =0 ;
+		return nbContrats;
+	}
 
+	
 }
